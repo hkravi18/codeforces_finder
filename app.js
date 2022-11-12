acaa833d89a24ac48ee2ecdb3dd938a4c60e1239
Harshit <hkravi2002@gmail.com>
2022-11-12 19:22:45 +0530

    second-commit

--- app.js
+++ app.js
@@ -258,9 +258,19 @@ app.get("/help",
     }  
 );
 
-app.listen(process.env.PORT || 3000, function (req, res) {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function (req,res) {
+    console.log("Server is running"); 
+});
+
+
+/*app.listen(process.env.PORT || 3000, function (req, res) {
     console.log("Server is running");
-}); 
+}); */
 
 
 
